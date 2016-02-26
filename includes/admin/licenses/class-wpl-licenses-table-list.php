251899c392ddce5aafe7113808ca02f730f206d1
Shiva Poudel <dev.shivapoudel@gmail.com>
2016-02-26 23:13:47 +0545

    No need to use data for fetching user

--- includes/admin/licenses/class-wpl-licenses-table-list.php
+++ includes/admin/licenses/class-wpl-licenses-table-list.php
@@ -137,7 +137,7 @@ class WPL_Licenses_Table_List extends WP_List_Table {
 			return '<span class="na">&ndash;</span>';
 		}
 
-		$user_name = ! empty( $user->data->display_name ) ? $user->data->display_name : $user->data->user_login;
+		$user_name = ! empty( $user->display_name ) ? $user->display_name : $user->user_login;
 
 		if ( current_user_can( 'edit_user' ) ) {
 			return '<a href="' . esc_url( add_query_arg( array( 'user_id' => $user->ID ), admin_url( 'user-edit.php' ) ) ) . '">' . esc_html( $user_name ) . '</a>';
